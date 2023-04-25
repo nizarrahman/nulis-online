1cb0e86bb87824ab319db0981eec971e29699adb
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2023-04-25 17:48:08 +0700

    Update main.js

--- app/main.js
+++ app/main.js
@@ -5,20 +5,20 @@ var router = express.Router()
 router.get('/nulis', async function (req, res, next) {
   res.render('index', {
     title:
-      'Nulis Online - ZaaDev',
-    my_url: "https://nulis-zaadev.vercel.app",
+      'Nulis Online - Nizar Rahman',
+    my_url: "https://nulis-nizar.vercel.app",
     desc: "Lagi mager nulis? mending Nulis Online aja, dijamin tangan gak bakalan pegel! aowkaowkokw",
-    author: "ZaaDev"
+    author: "Nizar"
   })
 })
 
 router.get('/', function (req, res) {
   res.render('home', {
     title:
-      'Nulis Online - ZaaDev',
-    my_url: "https://nulis-zaadev.vercel.app",
+      'Nulis Online - Nizar Rahman',
+    my_url: "https://nulis-nizar.vercel.app",
     desc: "Lagi mager nulis? mending Nulis Online aja, dijamin tangan gak bakalan pegel! aowkaowkokw",
-    author: "ZaaDev"
+    author: "Nizar"
   })
 })
 
